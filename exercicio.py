f1c6e333d57ca86217aed17bc4ee2c036440a6da
Wenderson <wenderson_fagundes@hotmail.com>
2024-05-01 19:24:36 -0300

    exercicio 21

--- exercicio.py
+++ exercicio.py
@@ -115,29 +115,38 @@
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-valor1 = True
-valor2 = False
-resultado_and = valor1 and valor2
-print("Resultado do AND lógico:", resultado_and)
+# valor1 = True
+# valor2 = False
+# resultado_and = valor1 and valor2
+# print("Resultado do AND lógico:", resultado_and)
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-resultado_or = valor1 or valor2
-print("Resultado do OR lógico:", resultado_or)
+# resultado_or = valor1 or valor2
+# print("Resultado do OR lógico:", resultado_or)
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-resulto_not = not valor1
-print("Resultado do NOT lógico: ", resulto_not)
+# resulto_not = not valor1
+# print("Resultado do NOT lógico: ", resulto_not)
 
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-comparacao = valor1 == valor2
-print("Resultado da comparação: ",comparacao)
+# comparacao = valor1 == valor2
+# print("Resultado da comparação: ",comparacao)
 
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
-diferente = valor1 != valor2
-print("Resultado da diferença: ", diferente)
+# diferente = valor1 != valor2
+# print("Resultado da diferença: ", diferente)
 
 # #### try-except e if
 
 # 21: Conversor de Temperatura
+
+try:
+    celsius = float(input("Digite um temperatura : "))  # Exemplo de entrada
+    fahrenheit = (celsius * 1.8) + 32
+    print(f"{celsius}°C é igual a {fahrenheit}°F")
+except:
+        print("Por favor digite um numero valido")
+
+
 # 22: Verificador de Palíndromo
 # 23: Calculadora Simples
 # 24: Classificador de Números
